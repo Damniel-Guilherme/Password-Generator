b6327416fc1a4341bfa599ef8a8e24d4ce2a8a2e
DAMNIEL <114008220+Damniel-Guilherme@users.noreply.github.com>
2024-01-06 19:09:58 -0300

    chode: update

--- script.js
+++ script.js
@@ -1,68 +1,67 @@
 let sliderElement = document.querySelector("#slider");
-let suasenha = document.querySelector("#suasenha")
+let suasenha = document.querySelector("#suasenha");
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
 let containerPassword = document.querySelector("#container-password");
 
-let novaSenha = '';
+console.log(containerPassword)
+
+let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
 sliderElement.oninput = function () {
-    sizePassword.innerHTML = this.value;
-}
+  sizePassword.innerHTML = this.value;
+};
 
-function generatePassword () {
+function generatePassword() {
+  const caracteresABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const caracteresAbc = "abcdefghijklmnopqrstuvwxyz";
+  const caracteresNumeros = "0123456789";
+  const caracteresSimbolos = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
+  let charset = "";
 
-    const letras = 'abcdefghijklmnopqrstuvwxyz';
-    const letrasMaiuscula = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const numeros = '0123456789';
-    const simbolos = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+  const letrasABC = document.getElementById("letrasABC").checked;
+  const letrasabc = document.getElementById("letrasabc").checked;
+  const numeros123 = document.getElementById("numeros123").checked;
+  const simbolos = document.getElementById("simbolos").checked;
 
-    let charset = '';
-    
-    const incluirLetras = document.getElementById('apenasLetras').checked;
-    const incluirLetrasMaisculunas = document.getElementById('apenasLetrasMaiusculas').checked;
-    const incluirNumeros = document.getElementById('apenasNumeros').checked;
-    const incluirSimbolos = document.getElementById('apenasSimbolos').checked;
+  if (letrasABC) {
+    charset += caracteresABC;
+  }
 
-    if (incluirLetras) {
-        charset += letras;
-    }
+  if (letrasabc) {
+    charset += caracteresAbc;
+  }
 
-    if (incluirNumeros) {
-        charset += numeros;
-    }
+  if (numeros123) {
+    charset += caracteresNumeros;
+  }
 
-    if (incluirSimbolos) {
-        charset += simbolos;
-    }
+  if (simbolos) {
+    charset += caracteresSimbolos;
+  }
 
-    if (incluirLetrasMaisculunas ) {
-        charset += letrasMaiuscula ;
-    }
-
-    if (charset === '') {
-        containerPassword.classList.remove("hide");
-        suasenha.innerHTML = 'Escolha uma categoria de caracteres!';
-        password.innerHTML = '❌❌❌❌❌❌';
-        return; // Retorna sem gerar a senha se nenhuma opção estiver selecionada
-    }
-
-    let pass = "";
-    for (let i = 0, n = charset.length;  i < sliderElement.value; ++i) {
-        pass += charset.charAt(Math.floor(Math.random() * n));
-    }  
-
+  if (charset === "") {
     containerPassword.classList.remove("hide");
-    suasenha.innerHTML = 'Sua senha gerada foi:';
-    password.innerHTML = pass;
-    novaSenha = pass;
-    
+    suasenha.innerHTML = "Choose a character category!";
+    password.innerHTML = "❌❌❌❌❌❌";
+    return; // Retorna sem gerar a senha se nenhuma opção estiver selecionada
+  }
+
+  let pass = "";
+  for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  containerPassword.classList.remove("hide");
+  suasenha.innerHTML = "Your generated password was:";
+  password.innerHTML = pass;
 }
 
-function copyPassword () {
-    navigator.clipboard.writeText(novaSenha);
-    alert("Senha copiada com sucesso!")
+function copyPassword(pass) {
+  novaSenha = pass;
+  navigator.clipboard.writeText(novaSenha);
+  alert("Senha copiada com sucesso!");
 }
